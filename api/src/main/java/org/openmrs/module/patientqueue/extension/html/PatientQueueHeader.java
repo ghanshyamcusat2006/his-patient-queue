80c62635c687e9918cc3b6f6e40f9c7475a44a2d
ghanshyamcusat2006 <ghanshyam.cusat2006@gmail.com>
2018-03-21 13:32:06 +0530

    redmine issue #11826 upgraded current HIS module based on OpenMRS 1.8.0 to OpenMRS 1.9.7

--- api/src/main/java/org/openmrs/module/patientqueue/extension/html/PatientQueueHeader.java
+++ api/src/main/java/org/openmrs/module/patientqueue/extension/html/PatientQueueHeader.java
@@ -20,9 +20,9 @@
 
 package org.openmrs.module.patientqueue.extension.html;
 
-import org.openmrs.module.Extension;
+import org.openmrs.module.web.extension.LinkExt;
 
-public class PatientQueueHeader extends Extension {
+public class PatientQueueHeader extends LinkExt {
 
 	public MEDIA_TYPE getMediaType() {
 		return MEDIA_TYPE.html;
